32ac6bc15391f7f8a12965573f46dd1c678e3020
wellingtonmnf <wellingtonmnf@outlook.com>
2024-12-19 09:07:16 -0300

    Atualização - Aula 280

--- src/test/java/academy/devdojo/maratonajava/javacore/junit/service/PersonServiceTest.java
+++ src/test/java/academy/devdojo/maratonajava/javacore/junit/service/PersonServiceTest.java
@@ -1,5 +1,8 @@
 package academy.devdojo.maratonajava.javacore.junit.service;
 
+import academy.devdojo.maratonajava.javacore.junit.dominio.Person;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,6 +10,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class PersonServiceTest {
 
     @Test
-    void isAdult() {
+    @DisplayName("A person should be not adult when age is lower than 18")
+    void isAdult_ReturnFalse_WhenAgeIsLowerThan18() {
+        Person person = new Person(15);
+        PersonService personService = new PersonService();
+//        Assertions.assertEquals(false, personService.isAdult(person));
+        assertFalse(personService.isAdult(person));
     }
 }
