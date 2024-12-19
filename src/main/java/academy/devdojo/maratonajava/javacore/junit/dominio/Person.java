8851231dfcf3e62b63ce17d76d237ec788459d67
wellingtonmnf <wellingtonmnf@outlook.com>
2024-12-18 21:22:42 -0300

    Atualização - Aula 280

---
+++ src/main/java/academy/devdojo/maratonajava/javacore/junit/dominio/Person.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.junit.dominio;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Person {
+    private int age;
+}
---
+++ src/main/java/academy/devdojo/maratonajava/javacore/junit/test/PersonServiceTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.junit.test;
+
+import academy.devdojo.maratonajava.javacore.junit.dominio.Person;
+import academy.devdojo.maratonajava.javacore.junit.service.PersonService;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class PersonServiceTest01 {
+
+    Person person = new Person(15);
+    PersonService personService = new PersonService();
+
+
+
+
+}
